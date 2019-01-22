204bf3629bfb84a9e732f39de1e4ce7ad93ec77f
Paola Vargas <vrgspaola68@gmail.com>
2019-01-22 11:51:51 -0400

    Create new form view for a cattle

--- rockets/keppler_frontend/app/assets/javascripts/keppler_frontend/app/main.js
+++ rockets/keppler_frontend/app/assets/javascripts/keppler_frontend/app/main.js
@@ -27,10 +27,3 @@ $('.multiple-items').slick({
 $('.single-item').slick({
   infinite: false
 });
-
-$('.keppler-switch').click(function(event) {
-  event.preventDefault()
-  $(this).find('input')
-    .toggleClass('active')
-    .attr('checked', $(this).find('input').hasClass('active'))
-})

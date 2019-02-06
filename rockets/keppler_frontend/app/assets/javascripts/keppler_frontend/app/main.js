f67f9fb879d855b357baa59c6ae25e5d4da3503e
Gladibeth <gladibethdelos@gmail>
2019-02-06 16:52:10 -0400

    new frontend

--- rockets/keppler_frontend/app/assets/javascripts/keppler_frontend/app/main.js
+++ rockets/keppler_frontend/app/assets/javascripts/keppler_frontend/app/main.js
@@ -1,9 +1,12 @@
-$('.multiple-items').slick({
+$('.items').slick({
   infinite: true,
   slidesToShow: 1,
   slidesToScroll: 1,
   nextArrow: '<button class="slick-next slick-arrow icofont-double-right"></button>',
   prevArrow: '<button class="slick-prev slick-arrow icofont-double-left"></button>',
+  asNavFor: '.multiple-items',
+  arrows: false,
+  dots: false,
   responsive: [
     {
       breakpoint: 480,
@@ -24,6 +27,36 @@ $('.multiple-items').slick({
   ]
 });
 
+$('.multiple-items').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: false,
+  asNavFor: '.items',
+  dots: true,
+  focusOnSelect: true
+});
+
+
+
 // $('.single-item').slick({
 //   infinite: false
 // });
+
+
+$(document).ready(function () {
+  WebFontConfig = {
+    google: {
+      families: ['Lato:300,400,500,600,700,800,900', 'Fira sans:900']
+    }
+  };
+  (function () {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
+
+});

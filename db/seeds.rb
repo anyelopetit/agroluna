40ebf6a80acafd6cbdd736b3cd8da03b04ba2482
Anyelo Petit <anyelopetit@gmail.com>
2019-04-30 11:05:51 -0400

    fixed race nil in modalBirth

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#   cities = City.create!([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create!(name: 'Emanuel', city: cities.first)
 # user = CreateAdminService.new.call
 # puts 'CREATED ADMIN USER: ' << user.email
 
@@ -16,7 +16,7 @@
 end
 
 
-User.create(
+User.create!(
   name: 'Keppler Admin', email: 'admin@keppleradmin.com', password: '+12345678+',
   password_confirmation: '+12345678+', role_ids: '1'
 )
@@ -26,7 +26,7 @@ puts 'admin@keppler.io has been created'
 
 # Create setting deafult
 if yml = YAML.load_file("#{Rails.root}/config/settings.yml")
-  setting = Setting.create(
+  setting = Setting.create!(
     name: yml['name'],
     description: yml['description'],
     email: yml['email'],
@@ -67,7 +67,7 @@ if yml = YAML.load_file("#{Rails.root}/config/settings.yml")
   setting.save!
   puts 'Setting has been created'
 else
-  setting = Setting.create(
+  setting = Setting.create!(
     name: 'Keppler Admin', description: 'Welcome to Keppler Admin',
     smtp_setting_attributes: {
       address: 'test', port: '25', domain_name: 'keppler.com',
@@ -89,7 +89,7 @@ if defined?(KepplerFrontend) && KepplerFrontend.is_a?(Module)
   file =  File.join("#{Rails.root}/rockets/keppler_frontend/config/data/views.yml")
   routes = YAML.load_file(file)
   routes.each do |route|
-    KepplerFrontend::View.create(
+    KepplerFrontend::View.create!(
       name: route['name'],
       url: route['url'],
       method: route['method'],
@@ -102,7 +102,7 @@ if defined?(KepplerFrontend) && KepplerFrontend.is_a?(Module)
   partials = YAML.load_file(partials_file)
 
   partials.each do |partial|
-    KepplerFrontend::Partial.create(
+    KepplerFrontend::Partial.create!(
       name: partial['name'],
     )
   end
@@ -114,7 +114,7 @@ if defined?(KepplerLanguages) && KepplerLanguages.is_a?(Module)
   langs = YAML.load_file(languages)
 
   langs.each do |lang|
-    KepplerLanguages::Language.create(
+    KepplerLanguages::Language.create!(
       name: lang['name']
     )
   end
@@ -123,7 +123,7 @@ if defined?(KepplerLanguages) && KepplerLanguages.is_a?(Module)
   fields = YAML.load_file(file_fields)
 
   fields.each do |field|
-    KepplerLanguages::Field.create(
+    KepplerLanguages::Field.create!(
       key: field['key'],
       value: field['value'],
       language_id: field['language_id']
@@ -137,7 +137,7 @@ if defined?(KepplerFrontend) && KepplerFrontend.is_a?(Module)
   funcs = YAML.load_file(functions)
 
   funcs.each do |func|
-    KepplerFrontend::Function.create(
+    KepplerFrontend::Function.create!(
       name: func['name'],
       description: func['description']
     )
@@ -147,7 +147,7 @@ if defined?(KepplerFrontend) && KepplerFrontend.is_a?(Module)
   params = YAML.load_file(parameters_file)
 
   params.each do |param|
-    KepplerFrontend::Parameter.create(
+    KepplerFrontend::Parameter.create!(
       name: param['name'],
       function_id: param['function_id']
     )
@@ -159,14 +159,14 @@ end
 
 # Fincas
 ['Agropecuaria La Luna', 'Finca Agrocabrita'].each do |farm|
-  farm = KepplerFarm::Farm.create(
+  farm = KepplerFarm::Farm.create!(
     title: farm,
     description: Faker::Lorem.paragraph
   )
 
   # Lotes
   6.times do |i|
-    KepplerFarm::StrategicLot.create(
+    KepplerFarm::StrategicLot.create!(
       name: Faker::Lorem.sentence(3, true, 3),
       function: Faker::Lorem.word,
       description: Faker::Lorem.paragraph,
@@ -178,24 +178,26 @@ puts 'Fincas y lotes creados'
 
 # Especies
 %i[Vacuno].each do |species_name|
-  species = KepplerCattle::Species.create(
+  species = KepplerCattle::Species.create!(
     name: species_name,
     abbreviation: species_name.to_s.remove('a', 'e', 'i', 'o', 'u')
   )
+  puts "Especie #{species_name} creada"
 
   # Razas
   30.times do |race_index|
-    KepplerCattle::Race.create(
+    KepplerCattle::Race.create!(
       name: "Raza #{species_name} #{race_index}",
       abbreviation: "R#{race_index}",
       description: Faker::Lorem.paragraph,
       species_id: species.id
     )
   end
+  puts "Razas de specie #{species_name} creadas"
 
   # Pesajes
   4.times do |weighing|
-    KepplerCattle::WeighingDay.create(
+    KepplerCattle::WeighingDay.create!(
       name: "Pesaje #{species_name} #{weighing}",
       min_days: (210..600).to_a.sample,
       specific_day: (210..600).to_a.sample,
@@ -203,22 +205,25 @@ puts 'Fincas y lotes creados'
       species_id: species.id
     )
   end
+  puts "Pesajes de #{species_name} creadas"
 
   # Condiciones corporales
   4.times do |corporal_condition|
-    KepplerCattle::CorporalCondition.create(
+    KepplerCattle::CorporalCondition.create!(
       name: "Condición Corporal #{species_name} #{corporal_condition}",
       number: corporal_condition,
       description: Faker::Lorem.paragraph,
       species_id: species.id
     )
   end
+  puts "Condiciones corporales de #{species_name} creadas"
   
   # Madre Sute
-  KepplerCattle::Cow.create(
+  KepplerCattle::Cow.create!(
     serie_number: '0000',
     short_name: 'Madre Sute',
     gender: 'female',
+    species_id: species.id,
     race_id: species.races.sample.id,
     birthdate: Faker::Date.backward(3650),
     coat_color: KepplerCattle::Cow.colors.sample,
@@ -234,7 +239,7 @@ puts 'Especies, razas y pesajes creados'
 
 # Tipologías
 ['BECERRO', 'MAUTE', 'NOVILLO', 'TORO'].each_with_index do |typology, index|
-  KepplerCattle::Typology.create(
+  KepplerCattle::Typology.create!(
     name: "#{typology}",
     abbreviation: typology.remove('a', 'e', 'i', 'o', 'u'),
     gender: 'male',
@@ -246,7 +251,7 @@ puts 'Especies, razas y pesajes creados'
 end
 
 ['BECERRA', 'MAUTA', 'NOVILLA', 'VACA'].each_with_index do |typology, index|
-  KepplerCattle::Typology.create(
+  KepplerCattle::Typology.create!(
     name: "#{typology}",
     abbreviation: typology.remove('a', 'e', 'i', 'o', 'u'),
     gender: 'female',
@@ -259,10 +264,17 @@ end
 end
 puts 'Tipologías creadas'
 
+5.times do |index|
+  KepplerFarm::Responsable.create!(
+    name: Faker::GameOfThrones.character
+  )
+end
+puts "Responsables creados"
+
 # Series
 100.times do |i|
   species = KepplerCattle::Species.all.sample
-  cow = KepplerCattle::Cow.create(
+  cow = KepplerCattle::Cow.create!(
     serie_number: Faker::Number.between(111111, 999999),
     short_name: Faker::Name.first_name,
     # farm_id: [1,2].sample,
@@ -281,7 +293,7 @@ puts 'Tipologías creadas'
   )
 
   farm = KepplerFarm::Farm.find([1,2].sample)
-  KepplerCattle::Location.create(
+  KepplerCattle::Location.create!(
     user_id: 1,
     cow_id: cow.id,
     farm_id: farm&.id,
@@ -289,25 +301,28 @@ puts 'Tipologías creadas'
   )
   
   if cow.gender?('male')
-    KepplerCattle::Male.create(
+    KepplerCattle::Male.create!(
+      user_id: User.all.sample.id,
       cow_id: cow.id,
       reproductive: [true, false].sample,
       defiant: [true, false].sample
     )
   else
-    KepplerCattle::Status.create(
+    KepplerCattle::Status.create!(
       status_type: 0,
       date: Faker::Date.backward(40),
       months: Faker::Number.between(1, 2),
-      user_id: User.all.sample
+      user_id: User.all.sample,
+      cow_id: cow.id
     )
   end
 
   # Pesaje
   5.times do |index|
     actual_weight = Faker::Number.decimal(3)
-    KepplerCattle::Weight.create(
-      user_id: 1,
+    responsable = KepplerFarm::Responsable.all.sample
+    KepplerCattle::Weight.create!(
+      user: responsable,
       cow_id: cow.id,
       weight: actual_weight.to_f,
       gained_weight: 26.04.nil? ? 0 : (actual_weight.to_f - 24.50),
@@ -316,7 +331,7 @@ puts 'Tipologías creadas'
       observations: Faker::Lorem.paragraph
     )
 
-    KepplerCattle::Activity.create(
+    KepplerCattle::Activity.create!(
       active: true,
       cow_id: cow.id,
       observations: Faker::Lorem.paragraph
@@ -324,7 +339,7 @@ puts 'Tipologías creadas'
   end
 
   # Pajuelas
-  KepplerCattle::Insemination.create(
+  KepplerCattle::Insemination.create!(
     serie_number: Faker::Number.between(111111, 999999),
     short_name: Faker::Name.first_name,
     farm_id: [1,2].sample,
@@ -347,7 +362,7 @@ puts 'Series creadas'
 # 2.times do |farm|
 #   puts "Llenando #{KepplerFarm::Farm.find(farm+1).title} de 20 transferencias "
 #   20.times do |transference|
-#     KepplerCattle::Transference.create(
+#     KepplerCattle::Transference.create!(
 #       cattle: KepplerFarm::Farm.find_by(id: farm+1).cows.take(transference).pluck(:id),#take([1..20].sample),
 #       from_farm_id: farm.eql?(1) ? 1 : 2,
 #       to_farm_id: farm.eql?(1) ? 2 : 1,
@@ -361,7 +376,7 @@ puts 'Series creadas'
 species = KepplerCattle::Species.first
 
 8.times do |index|
-  cow = KepplerCattle::Cow.create(
+  cow = KepplerCattle::Cow.create!(
     serie_number: ((index+1)*11111111),
     short_name: "Bisabuelo",#{(index % 2).zero? ? 'o' : 'a'}",
     # farm_id: [1,2].sample,
@@ -379,7 +394,7 @@ species = KepplerCattle::Species.first
     father_id: KepplerCattle::Cow.where(gender: 'male')&.sample&.id
   )
   actual_weight = Faker::Number.decimal(3)
-  KepplerCattle::Weight.create(
+  KepplerCattle::Weight.create!(
     user_id: 1,
     cow_id: cow.id,
     weight: actual_weight.to_f,
@@ -389,14 +404,14 @@ species = KepplerCattle::Species.first
     observations: Faker::Lorem.paragraph
   )
 
-  KepplerCattle::Activity.create(
+  KepplerCattle::Activity.create!(
     active: true,
     cow_id: cow.id,
     observations: Faker::Lorem.paragraph
   )
 
   farm = KepplerFarm::Farm.find([1,2].sample)
-  KepplerCattle::Location.create(
+  KepplerCattle::Location.create!(
     user_id: 1,
     cow_id: cow.id,
     farm_id: farm&.id,
@@ -406,7 +421,7 @@ end
 puts 'Bisabuelos creados'
 
 %w[11112222 33334444 55556666 77778888].each_with_index do |serie_number, index|
-  cow = KepplerCattle::Cow.create(
+  cow = KepplerCattle::Cow.create!(
     serie_number: serie_number,
     short_name: "Abuelo",#{index%2.zero? ? 'o' : 'a'}",
     # farm_id: [1,2].sample,
@@ -424,7 +439,7 @@ puts 'Bisabuelos creados'
     mother_id: KepplerCattle::Cow.find_by(serie_number: serie_number.last*8).id
   )
   actual_weight = Faker::Number.decimal(2)
-  KepplerCattle::Weight.create(
+  KepplerCattle::Weight.create!(
     user_id: 1,
     cow_id: cow.id,
     weight: actual_weight.to_f,
@@ -434,14 +449,14 @@ puts 'Bisabuelos creados'
     observations: Faker::Lorem.paragraph
   )
 
-  KepplerCattle::Activity.create(
+  KepplerCattle::Activity.create!(
     active: true,
     cow_id: cow.id,
     observations: Faker::Lorem.paragraph
   )
 
   farm = KepplerFarm::Farm.find([1,2].sample)
-  KepplerCattle::Location.create(
+  KepplerCattle::Location.create!(
     user_id: 1,
     cow_id: cow.id,
     farm_id: farm&.id,
@@ -451,7 +466,7 @@ end
 puts 'Abuelos creados'
 
 %w[11223344 55667788].each_with_index do |serie_number, index|
-  cow = KepplerCattle::Cow.create(
+  cow = KepplerCattle::Cow.create!(
     serie_number: serie_number,
     short_name: "#{['Padre', 'Madre'][index%2]}",
     # farm_id: [1,2].sample,
@@ -469,7 +484,7 @@ puts 'Abuelos creados'
     mother_id: KepplerCattle::Cow.find_by(serie_number: serie_number[4..5]*2 + serie_number[6..7]*2).id
   )
   actual_weight = Faker::Number.decimal(2)
-  KepplerCattle::Weight.create(
+  KepplerCattle::Weight.create!(
     user_id: 1,
     cow_id: cow.id,
     weight: actual_weight.to_f,
@@ -479,14 +494,14 @@ puts 'Abuelos creados'
     observations: Faker::Lorem.paragraph
   )
 
-  KepplerCattle::Activity.create(
+  KepplerCattle::Activity.create!(
     active: true,
     cow_id: cow.id,
     observations: Faker::Lorem.paragraph
   )
 
   farm = KepplerFarm::Farm.find([1,2].sample)
-  KepplerCattle::Location.create(
+  KepplerCattle::Location.create!(
     user_id: 1,
     cow_id: cow.id,
     farm_id: farm&.id,
@@ -496,7 +511,7 @@ end
 puts 'Padres creados'
 
 
-cow = KepplerCattle::Cow.create(
+cow = KepplerCattle::Cow.create!(
   serie_number: '12345678',
   short_name: "Hijo",
   # farm_id: [1,2].sample,
@@ -514,7 +529,7 @@ cow = KepplerCattle::Cow.create(
   mother_id: KepplerCattle::Cow.find_by(serie_number: "55667788").id
 )
 actual_weight = Faker::Number.decimal(2)
-KepplerCattle::Weight.create(
+KepplerCattle::Weight.create!(
   user_id: 1,
   cow_id: cow.id,
   weight: (50.05 * 1),
@@ -524,14 +539,14 @@ KepplerCattle::Weight.create(
   observations: Faker::Lorem.paragraph
 )
 
-KepplerCattle::Activity.create(
+KepplerCattle::Activity.create!(
   active: true,
   cow_id: cow.id,
   observations: Faker::Lorem.paragraph
 )
 
 farm = KepplerFarm::Farm.all.sample
-KepplerCattle::Location.create(
+KepplerCattle::Location.create!(
   user_id: 1,
   cow_id: cow.id,
   farm_id: farm&.id,

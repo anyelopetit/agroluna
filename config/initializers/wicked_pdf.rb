1eddb38c06f1140a56820082256a2f2f6233fc92
Anyelo Petit <anyelopetit@gmail.com>
2019-05-28 09:57:19 -0400

    changed last.day to last&.day

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -8,12 +8,24 @@
 #
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
 
+require 'rbconfig'
+
+if RbConfig::CONFIG['host_os'] =~ /linux/
+  arch = RbConfig::CONFIG['host_cpu'] == 'x86_64' ? 'wkhtmltopdf_linux_x64' : 'wkhtmltopdf_linux_386'
+elsif RbConfig::CONFIG['host_os'] =~ /darwin/
+  arch = 'wkhtmltopdf_darwin_386'
+else
+  raise "Invalid platform. Must be running Intel-based Linux or OSX."
+end
+
 WickedPdf.config = {
   # Path to the wkhtmltopdf executable: This usually isn't needed if using
   # one of the wkhtmltopdf-binary family of gems.
   # exe_path: '/usr/local/bin/wkhtmltopdf',
+  # exe_path: '/usr/local/bin/wkhtmltopdf'
   #   or
   # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
+  # exe_path: "#{ENV['GEM_HOME']}/gems/wkhtmltopdf-binary-#{Gem.loaded_specs['wkhtmltopdf-binary'].version}/bin/#{arch}"
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
